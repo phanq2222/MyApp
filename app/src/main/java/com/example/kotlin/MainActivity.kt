d071b13453fc0b74e007ee998c24a89f36379825
phanq2222 <88728017+phanq2222@users.noreply.github.com>
2022-08-18 16:53:49 +0700

    update code

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -13,25 +13,35 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
 
         findViewById<Button>(R.id.btnTextColor).setOnClickListener {
             findViewById<TextView>(R.id.textView).setTextColor(getResources().getColor(R.color.red))
         }
        findViewById<Button>(R.id.btnChangeView).setOnClickListener {
-           findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(getResources().getColor(R.color.cornsilk))
+           findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(getResources().getColor(R.color.dark_cyan))
        }
        findViewById<Button>(R.id.btnChangeText).setOnClickListener {
            val strValue: String = findViewById<EditText>(R.id.editText).text.toString()
-           val strTextView: String =  findViewById<TextView>(R.id.textView).text.toString()
            if(TextUtils.isEmpty(strValue))
            {
-                findViewById<TextView>(R.id.textView).text = strTextView
+                findViewById<TextView>(R.id.textView).setText(resources.getString(R.string.text_view_default))
            }
            else
             findViewById<TextView>(R.id.textView).text = strValue
+       }
+       findViewById<ConstraintLayout>(R.id.backgroundView).setOnClickListener{
+            findViewById<ConstraintLayout>(R.id.backgroundView).setBackgroundColor(resources.getColor(R.color.design_default_color_background))
+           findViewById<TextView>(R.id.textView).setText(resources.getString(R.string.text_view_default))
+           findViewById<TextView>(R.id.textView).setTextColor(resources.getColor(R.color.black))
+           findViewById<EditText>(R.id.editText).setText(resources.getString(R.string.edit_text_default))
        }
     }
 }
